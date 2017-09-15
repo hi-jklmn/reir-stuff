f728d4d864d537d9f03aafa03d7c90e705d0a259
SoYman <aarnaldur@gmail.com>
2017-09-15 22:41:35 +0000

    Fast2 is borne

--- REIR/src/s2/Fast2.java
+++ REIR/src/s2/Fast2.java
@@ -16,7 +16,19 @@ public class Fast2 {
             points[i] = new Point(x, y);
         }
         // Process stuff
-        String output = "";
+        Arrays.sort(points);
+        Point[] sorted = Arrays.copyOf(points, n);
+        for (int i = 0; i < n - 3; i++) {
+            Point p = points[i];
+            Arrays.sort(sorted, p.SLOPE_ORDER);
+
+            for (int j = 2; j < n; j++) {
+                double currentSlope = p.slopeTo(sorted[j]);
+                if (currentSlope == p.slopeTo(sorted[j - 2])) {
+
+                }
+            }
+        }
         while (points.length > 3) {
             Arrays.sort(points);
             Point p = points[0];
@@ -25,8 +37,7 @@ public class Fast2 {
             Arrays.sort(points, p.SLOPE_ORDER);
 
             for (int i = 2; i < points.length; i++) {
-                if (p.slopeTo(points[i - 2]) == p.slopeTo(points[i - 1]) &&
-                        p.slopeTo(points[i - 1]) == p.slopeTo(points[i])) {
+                if (p.slopeTo(points[i - 2]) == p.slopeTo(points[i])) {
                     StdOut.print(p.toString());
                     int j = i - 2;
                     while (j < points.length && p.slopeTo(points[j]) == p.slopeTo(points[i])) {
