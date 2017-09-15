b17c366fc04f2ba3098ba057bdbc8945ce78cf83
Jökull Máni <jokull16@ru.is>
2017-09-15 22:59:24 +0000

    fast2

--- REIR/src/s2/Fast2.java
+++ REIR/src/s2/Fast2.java
@@ -17,18 +17,42 @@ public class Fast2 {
         }
         // Process stuff
         Arrays.sort(points);
+        
         Point[] sorted = Arrays.copyOf(points, n);
         for (int i = 0; i < n - 3; i++) {
             Point p = points[i];
             Arrays.sort(sorted, p.SLOPE_ORDER);
-
+            
             for (int j = 2; j < n; j++) {
+            		
+            		String current_line = "";
+            	
                 double currentSlope = p.slopeTo(sorted[j]);
+                
                 if (currentSlope == p.slopeTo(sorted[j - 2])) {
-
+                		
+                		int k;
+                		for(k = 0; j+k+1 < n && currentSlope == p.slopeTo(sorted[j+k+1]); k++);
+                		
+                		current_line += p.toString();
+                		
+                		for(int l = j-2; l < j+k; l++) {
+                			if(p.compareTo(sorted[l]) == -1) {
+                				current_line += " -> " + sorted[l].toString();
+                			} else {
+                				i = j+k;
+                				current_line = "";
+                				continue;
+                			}
+                		}
+                }
+                
+                if(current_line.length() > 0) {
+                		StdOut.println(current_line);
                 }
             }
         }
+        
         while (points.length > 3) {
             Arrays.sort(points);
             Point p = points[0];
