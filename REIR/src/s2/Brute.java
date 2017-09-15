8e4337b138fc365815f9f3addc43551915719390
SoYman <aarnaldur@gmail.com>
2017-09-15 18:32:48 +0000

    It's so fast

--- REIR/src/s2/Brute.java
+++ REIR/src/s2/Brute.java
@@ -1,5 +1,42 @@
 package s2;
 
-public class Brute {
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Out;
+import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
+public class Brute {
+    //et tu brute
+    public static void main(String[] args) {
+        // Input handling
+        In in = new In();
+        Out out = new Out();
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt(), y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+        Arrays.sort(points);
+        // Process stuff
+
+        for (int i = 0; i < n - 3; i++) {
+            for (int j = i + 1; j < n - 2; j++) {
+                for (int k = j + 1; k < n - 1; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        double a = points[i].slopeTo(points[j]);
+                        double b = points[j].slopeTo(points[k]);
+                        double c = points[k].slopeTo(points[l]);
+                        if (a == b && b == c) {
+                            StdOut.print(points[i].toString() + " -> ");
+                            StdOut.print(points[j].toString() + " -> ");
+                            StdOut.print(points[k].toString() + " -> ");
+                            StdOut.println(points[l].toString());
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- REIR/src/s2/Fast.java
+++ REIR/src/s2/Fast.java
@@ -1,5 +1,49 @@
 package s2;
 
-public class Fast {
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Out;
+import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
+public class Fast {
+    public static void main(String[] args) {
+        // Input handling
+        In in = new In();
+        Out out = new Out();
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt(), y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+        // Process stuff
+
+        while (points.length > 1) {
+            Arrays.sort(points);
+            Point p = points[0];
+            points = Arrays.copyOfRange(points, 1, points.length);
+
+            Arrays.sort(points, p.SLOPE_ORDER);
+
+            for (int i = 2; i < points.length; i++) {
+                if (p.slopeTo(points[i - 2]) == p.slopeTo(points[i - 1]) &&
+                        p.slopeTo(points[i - 1]) == p.slopeTo(points[i])) {
+                    StdOut.print(p.toString() + " -> ");
+                    StdOut.print(points[i - 2].toString() + " -> ");
+                    StdOut.print(points[i - 1].toString() + " -> ");
+                    StdOut.print(points[i].toString());
+                    int j = i + 1;
+                    while (j < points.length && p.slopeTo(points[j]) == p.slopeTo(points[i])) {
+                        StdOut.print(" -> " + points[j].toString());
+                        j++;
+                    }
+                    i = j - 1;
+
+                    StdOut.println();
+                }
+            }
+
+        }
+    }
 }
