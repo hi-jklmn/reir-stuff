cfb755033168ef497418e0955253358c045874f5
SoYman <aarnaldur@gmail.com>
2017-09-15 19:56:39 +0000

    it work

--- REIR/src/s2/Fast.java
+++ REIR/src/s2/Fast.java
@@ -1,7 +1,6 @@
 package s2;
 
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Out;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Arrays;
@@ -10,7 +9,6 @@ public class Fast {
     public static void main(String[] args) {
         // Input handling
         In in = new In();
-        Out out = new Out();
         int n = in.readInt();
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
@@ -18,32 +16,50 @@ public class Fast {
             points[i] = new Point(x, y);
         }
         // Process stuff
-
-        while (points.length > 1) {
+        String output = "";
+        while (points.length > 3) {
             Arrays.sort(points);
             Point p = points[0];
             points = Arrays.copyOfRange(points, 1, points.length);
 
             Arrays.sort(points, p.SLOPE_ORDER);
 
+//            int run = 0;
+//            double current = p.slopeTo(points[0]);
+//            for (int i = 1; i < points.length; i++) {
+//                if (current == p.slopeTo(points[i])) {
+//                    run++;
+//                } else {
+//                    current = p.slopeTo(points[i]);
+//                    if (run > 0) {
+//                        output += p.toString();
+//                        for (int j = i-run; j < i; j++) {
+//                            output += " -> ";
+//                            output += points[j].toString();
+//                        }
+//                        output += "\n";
+//                    }
+//                    run = 0;
+//                }
+//            }
+
             for (int i = 2; i < points.length; i++) {
                 if (p.slopeTo(points[i - 2]) == p.slopeTo(points[i - 1]) &&
                         p.slopeTo(points[i - 1]) == p.slopeTo(points[i])) {
-                    StdOut.print(p.toString() + " -> ");
-                    StdOut.print(points[i - 2].toString() + " -> ");
-                    StdOut.print(points[i - 1].toString() + " -> ");
-                    StdOut.print(points[i].toString());
+                    output += (p.toString() + " -> ");
+                    output += (points[i - 2].toString() + " -> ");
+                    output += (points[i - 1].toString() + " -> ");
+                    output += (points[i].toString());
                     int j = i + 1;
                     while (j < points.length && p.slopeTo(points[j]) == p.slopeTo(points[i])) {
-                        StdOut.print(" -> " + points[j].toString());
+                        output += (" -> " + points[j].toString());
                         j++;
                     }
                     i = j - 1;
-
-                    StdOut.println();
+                    output += "\n";
                 }
             }
-
         }
+        StdOut.print(output);
     }
 }
