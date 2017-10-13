7a6008fbd2ef9a973f46c68a229c6f077355c647
Jökull Reynisson <jokullmani12@gmail.com>
2017-10-13 21:45:23 +0000

    WORDNET CONSTRUCTUOUOUOUOUR

--- REIR/src/s4/Outcast.java
+++ REIR/src/s4/Outcast.java
@@ -4,14 +4,41 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Outcast {
+
+    WordNet net;
+
     // constructor takes a WordNet object
     public Outcast(WordNet wordnet) {
-
+        net = wordnet;
     }
 
     // given an array of WordNet nouns , return an outcast
     public String outcast(String[] nouns) {
-        return "DICK";
+
+        int[][] lengths = new int[nouns.length][nouns.length];
+
+        for(int i = 0; i < nouns.length; i++) {
+            for(int j = i+1; j < nouns.length; j++) {
+                lengths[i][j] = net.distance(nouns[i], nouns[j]);
+                lengths[j][i] = lengths[i][j];
+            }
+        }
+
+        int candidate = 0;
+        int can_len = 0;
+
+        for(int i = 0; i < nouns.length; i++) {
+            int len = 0;
+            for(int j = 0; j < nouns.length; j++) {
+                len += lengths[i][j];
+            }
+            if(len > can_len) {
+                can_len = len;
+                candidate = i;
+            }
+        }
+
+        return nouns[candidate];
     }
 
     public static void main(String[] args) {
