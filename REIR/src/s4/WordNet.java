f9be8ae6dbaeedeb1e34b2b8eb4ae1a8ad9b3262
Jökull Reynisson <jokullmani12@gmail.com>
2017-10-13 18:01:47 +0000

    WORDNET CONSTRUCTUOUOUOUOUR

--- REIR/src/s4/WordNet.java
+++ REIR/src/s4/WordNet.java
@@ -1,10 +1,48 @@
 package s4;
 
 import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.LinearProbingHashST;
+
+import java.io.File;
 
 public class WordNet {
+
+    SAP hypernyms;
+    LinearProbingHashST<String, Integer> synsets = new LinearProbingHashST<>();
+
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
+        In in = new In(synsets);
+
+        int S = 0;
+
+        while(in.hasNextLine()) {
+            S++;
+            String[] curr = in.readLine().split(",");
+            String[] syns = curr[1].split(" ");
+
+            for(int i = 0; i < syns.length; i++) {
+                this.synsets.put(syns[i], Integer.valueOf(curr[0]));
+            }
+        }
+
+        Digraph G = new Digraph(S);
+
+        in = new In(hypernyms);
+
+        while(in.hasNextLine()) {
+            String[] curr = in.readLine().split(",");
+
+            int v = Integer.valueOf(curr[0]);
+
+            for(int i = 1; i < curr.length; i++) {
+                G.addEdge(v, Integer.valueOf(curr[i]));
+            }
+        }
+
+        this.hypernyms = new SAP(G);
     }
 
     // returns all WordNet nouns
